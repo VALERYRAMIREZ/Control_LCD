1da0ebe2de9a203538b1e8bf4c922e9a3f02fc0c
valery <valery@DESKTOP-3NE0SHT>
2019-09-30 12:09:23 -0400

    Se han colocado las definciones de terminales para la pantalla LCD y la función de inicialización, falta por probarla.

--- LCD_44780.h
+++ LCD_44780.h
@@ -13,8 +13,22 @@
 
 /* Definición de terminales para el control de la pantalla LCD. */
 
-#define             
+#define         RS      PORTDbits.RD0
+#define         RW      PORTDbits.RD1
+#define         E       PORTDbits.RD2
+#define         D7      PORTDbits.RD3
+#define         D6      PORTDbits.RD4
+#define         D5      PORTDbits.RD5
+#define         D4      PORTDbits.RD6
+#define         D3      PORTDbits.RD7
+#define         D2      PORTDbits.RD8
+#define         D1      PORTDbits.RD9
+#define         D0      PORTDbits.RD10
 
+/* Definición de prototipos de función para el control de la pantalla LCD.    */
+
+void Inicia_LCD4(unsigned short dato,bool tipo);/* Prototipo de función para
+                                             * inicializar la pantalla LCD.   */
 
 #endif	LCD_44780_H */
 
