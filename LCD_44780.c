1473779f1b1b48e19463ef2be7cd0570c3975fb8
valery <valery@DESKTOP-3NE0SHT>
2019-11-19 21:52:12 -0400

    Cambiado el cursor a apagado cuando se establece el modo de entrada de datos y agregada la función (sin probar) para desplazar el cursor hacia una dirección.

--- LCD_44780.c
+++ LCD_44780.c
@@ -154,9 +154,38 @@ void Inicia_LCD4(unsigned short dato)       /* función para inicializar la    */
     RW = 0;
     retardo_us(1);
     E = 1;
-    D7 = 1;                             /* Cursor encendido.                  */
+    D7 = 0;                             /* Cursor encendido.                  */
     D6 = 0;                    
     D5 = 1;                             /* Incremento del cursor.             */
+    D4 = 1;                             /* Desplazamiento del display al      */
+    retardo_us(1);                      /* aparecer la data.                  */
+    E = 0;
+}
+
+void Desplaza_Cur(char dir, char salto)
+{
+    retardo_us(100);                        /* Desplazamiento del cursor hacia*/
+                                            /* una dirección: dir = i = i el  */
+    RS = 0;                                 /* dir = i, cursor se mueve a la  */
+    RW = 0;                                 /*          izquierda.            */
+    retardo_us(1);                          /* dir = d, cursor se mueve a la  */
+    E = 1;                                  /*          derecha.              */
+    D7 = 0;
+    D6 = 0;
+    D5 = 1;
+    D4 = 1;
+    retardo_us(1);
+    E = 0;
+    
+    retardo_us(100);                  
+    
+    RS = 0;
+    RW = 0;
+    retardo_us(1);
+    E = 1;
+    D7 = 0;                             /* Cursor encendido.                  */
+    D6 = 1;                    
+    D5 = 0;                             /* Incremento del cursor.             */
     D4 = 1;                             /* Desplazamiento del display al      */
     retardo_us(1);                      /* aparecer la data.                  */
     E = 0;
--- LCD_44780.h
+++ LCD_44780.h
@@ -30,5 +30,8 @@
 void Inicia_LCD4(unsigned short dato);      /* Prototipo de función para
                                              * inicializar la pantalla LCD.   */
 
+void Desplaza_Cur(char dir, char salto);    /* Prototipo de función para mover
+                                             * el cursor hacia una dirección. */
+
 #endif	/* LCD_44780_H */
 
