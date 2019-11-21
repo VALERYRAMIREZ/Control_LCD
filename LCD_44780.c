af01c79b6be40ea71a82a24eff62a579f73a4f73
valery <valery@DESKTOP-3NE0SHT>
2019-11-20 21:06:13 -0400

    Cambiada la forma en la que se activan los terminales de control y datos de la pantalla LCD.

--- LCD_44780.c
+++ LCD_44780.c
@@ -6,159 +6,159 @@
 void Inicia_LCD4(unsigned short dato)       /* función para inicializar la    */
 {                                           /* pantalla LCD.                  */
     retardo_ms(16);                         /* Inicialización y espera.       */
-    
+    E = 0; 
+    retardo_us(5);
+    E = 1;
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_ms(10);
-    
+
+    E = 1;
+    retardo_us(5);    
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);
-    
+
+    E = 1; 
+    retardo_us(5);   
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);
-    
+
+    E = 1; 
+    retardo_us(5);   
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);
-    
+
+    E = 1;   
+    retardo_us(5); 
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 0;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;  
     
     retardo_ms(100);                        /* Número de líneas y tipo de     */
                                             /* fuente.      */
+    E = 1;
+    retardo_us(5);
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 1;
     D6 = 0;
     D5 = 0;
     D4 = 0;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);                        /* Se apaga el LCD.               */
-    
+
+    E = 1;  
+    retardo_us(5);  
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);                  
-    
+
+    E = 1; 
+    retardo_us(5);   
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 1;
     D6 = 0;
     D5 = 0;
     D4 = 0;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
 
     retardo_us(100);                        /* Se enciende el LCD.            */
-    
+
+    E = 1; 
+    retardo_us(5);   
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);                  
-    
+
+    E = 1;       
+    retardo_us(5); 
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 0;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);                        /* Configuración del modo de
                                              *  entrada.                      */
+    E = 1; 
+    retardo_us(5);   
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;
     D6 = 0;
     D5 = 1;
     D4 = 1;
-    retardo_us(1);
+    retardo_us(5);
     E = 0;
     
     retardo_us(100);                  
-    
+
+    E = 1;     
+    retardo_us(5);   
     RS = 0;
     RW = 0;
-    retardo_us(1);
-    E = 1;
     D7 = 0;                             /* Cursor encendido.                  */
     D6 = 0;                    
     D5 = 1;                             /* Incremento del cursor.             */
     D4 = 1;                             /* Desplazamiento del display al      */
-    retardo_us(1);                      /* aparecer la data.                  */
+    retardo_us(5);                      /* aparecer la data.                  */
     E = 0;
 }
 
