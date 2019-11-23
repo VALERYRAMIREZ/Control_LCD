e414601ad967a1e95ab305b7068456b970bf8938
valery <valery@DESKTOP-3NE0SHT>
2019-11-22 21:33:01 -0400

    Se modificaron los puertos y las funciones de manejo de puertos para inicializar el LCD.

--- LCD_44780.h
+++ LCD_44780.h
@@ -13,17 +13,17 @@
 
 /* Definición de terminales para el control de la pantalla LCD. */
 
-#define         RS      PORTDbits.RD0
-#define         RW      PORTDbits.RD1
-#define         E       PORTDbits.RD2
-#define         D7      PORTDbits.RD3
-#define         D6      PORTDbits.RD4
+#define         RS      PORTDbits.RD10
+#define         RW      PORTDbits.RD9
+#define         E       PORTDbits.RD8
+#define         D7      PORTDbits.RD7
+#define         D6      PORTDbits.RD6
 #define         D5      PORTDbits.RD5
-#define         D4      PORTDbits.RD6
-#define         D3      PORTDbits.RD7
-#define         D2      PORTDbits.RD8
-#define         D1      PORTDbits.RD9
-#define         D0      PORTDbits.RD10
+#define         D4      PORTDbits.RD4
+#define         D3      PORTDbits.RD3
+#define         D2      PORTDbits.RD2
+#define         D1      PORTDbits.RD1
+#define         D0      PORTDbits.RD0
 
 /* Definición de prototipos de función para el control de la pantalla LCD.    */
 
