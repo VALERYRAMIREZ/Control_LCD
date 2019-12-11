83f5182fa0816c8b7883d9399a2b218e91680e6f
valery <valery@DESKTOP-3NE0SHT>
2019-12-11 09:50:05 -0400

    Agregada la función para configurar todos los puertos (por ahora solo configura los puerto D y E).

--- E_S.c
+++ E_S.c
@@ -7,9 +7,20 @@
 
 #include "E_S.h"
 
-void Confi_PuertoD(unsigned int pinesD) /* Función para configurar el
-                                         * funcionamiento del puerto D. */
-{
+void Confi_PuertoD(unsigned int pinesD) /* Función para configurar el         */
+{                                       /* funcionamiento del puerto D.       */
     TRISD = pinesD;
     ODCD = 0;
+}
+
+void Confi_PuertoE(unsigned char pinesE)/* Función para configurar el         */
+{                                       /* funcionamiento del puerto E.       */
+    TRISE = pinesE;
+    ODCE = 0;
+}
+
+void Confi_E_S(void)                    /* Función para configurar el         */
+{                                       /* funcionamiento de los puertos.     */
+    Confi_PuertoD(0b1111000000000000);
+    Confi_PuertoE(0b11110000);
 }
