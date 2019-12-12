4db88ccb584d289217b74706faf119e553fa7646
valery <valery@DESKTOP-3NE0SHT>
2019-12-12 16:50:35 -0400

    Se comenzó la escritura de la función para manejar la interrupción por notificación de cambio en los pines seleccionados, por el momento solo los pines para el teclado.

--- interrup.c
+++ interrup.c
@@ -9,13 +9,26 @@ void Inicia_Interr(void)                    /* Función para configurar e iniciar
     IFS0bits.T3IF = 0;                      /* interrupción en 1 y se borra la*/
                                             /* y se borra la bandera de       */
                                             /* del timer de 32 bits.          */
+    
+    CNEN4 = 0xc000;                     /* Se habilitan los pines CN62 a CN65 */
+    CNEN5 = 0xc003;                     /* para detectar el cambio de estado, */
+                                        /* esto es, los Puerto E,4 a Puerto   */
+                                        /* E,7 para detectar la tecla         */
+                                        /* presionada.                        */
 }                                           
 
 
 /* Funciones para el manejo de interrupciones */
 
-void __attribute__((interrupt(auto_psv))) _T3Interrupt(void)/* Función para   */
+void __attribute__((interrupt(no_auto_psv))) _T3Interrupt(void)/* Función para*/
 {                                           /* manejo de la interrupción del  */
     T2CONbits.TON = 0;                      /* timer 32 para usarlo como      */
     IFS0bits.T3IF = 0;                      /* temporizador de 32 bits.       */
+}
+
+void __attribute__((interrupt(no_auto_psv))) _CNInterrupt(void)/* Función para*/
+{                                       /* el manejo de la interrupción por   */
+                                        /* cambio de estado en los terminales */
+                                        /* habilitados para este tipo de      */
+                                        /* interrupción.                      */
 }
