687c03465975b47d1f8541bf252da9a01980d05c
valery <valery@DESKTOP-3NE0SHT>
2020-01-24 17:26:21 -0400

    Removido el nombre de la estructura para evitar hacer llamados a nombres tan grandes.

--- main.c
+++ main.c
@@ -20,7 +20,12 @@ int main(void) {
     Inicia_Interr();
     Inicia_LCD4();
     Posicion_Cur4b(1,1);
-    Esc_DCRAM4b("El menú para hoy",2);
+    Ciclo_Timer1(100,0b0000000000000010);
+    PORTDbits.RD11 = 1;
+    Inicia_Ciclo_Timer1();
+    while(IFS0bits.T1IF);
+    PORTDbits.RD11 = 0;
+    Detiene_Ciclo_Timer1();
     while(1);
     return 0;
 }
